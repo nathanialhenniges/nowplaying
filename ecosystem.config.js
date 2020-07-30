6c39e0cc20460fcec7315e3a489fdce9b0dc7906
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-07-30 10:29:55 -0500

    Fixed deployed.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'MrDemonWolf-Share',
-      script: 'npm start',
+      name: 'NowPaying',
+      script: './index.js',
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
       exec_mode: 'cluster',
-      ignore_watch: '[”[/\\]./”, “node_modules”]',
       autorestart: true,
       max_memory_restart: '300M',
       env: {
